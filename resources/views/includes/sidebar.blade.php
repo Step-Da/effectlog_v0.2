8b492a5c940c721bbde2ce0a26f72db55f5e26bb
StepDa <p_d.a.stepanov>
2021-01-18 19:30:24 +0300

    add a general view of the site

---
+++ resources/views/includes/sidebar.blade.php
@@ -0,0 +1,45 @@
+<nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+    <div class="sidebar-sticky pt-3">
+      <ul class="nav flex-column">
+        <li class="nav-item">
+          <a class="nav-link" href="#">
+            <span><i class="fas fa-users sidebar-icon"></i></span>
+            Поставщики
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">
+            <span><i class="far fa-folder-open sidebar-icon"></i></span>
+            Отчеты
+          </a>
+        </li>
+      </ul>
+
+      <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+        <span>Быстрый доступ к отчётам</span>
+        <a class="d-flex align-items-center text-muted" href="#" aria-label="Add a new report">
+          <span data-feather="plus-circle"></span>
+        </a>
+      </h6>
+      <ul class="nav flex-column mb-2">
+        <li class="nav-item">
+          <a class="nav-link" href="#">
+           <span><i class="fas fa-file-signature sidebar-icon"></i></span>
+            Первый отчёт
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">
+            <span><i class="fas fa-file-signature sidebar-icon"></i></span>
+            Второй отчёт
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">
+            <span><i class="fas fa-file-signature sidebar-icon"></i></span>
+            Третий отчёт
+          </a>
+        </li>
+      </ul>
+    </div>
+  </nav>
--- routes/web.php
+++ routes/web.php
@@ -11,12 +11,13 @@
 |
 */
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', function () {
+    return view('home');
+})->name('homePage');
+
+Route::get('/auth', 'AuthController@index')->name('authorization');
