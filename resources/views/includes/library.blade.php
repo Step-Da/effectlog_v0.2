488373b25aa227b7945f3ab8bdb0549cab2f4f81
StepDa <p_d.a.stepanov>
2021-02-07 13:27:24 +0300

    fix library unit

--- resources/views/includes/library.blade.php
+++ resources/views/includes/library.blade.php
@@ -1,17 +1,17 @@
-@foreach ($data as $element)
-<div class="search">
-  <div class="container">
-    <div class="row">
-      <div class="col-md-4">
+<div class="container">
+  <div class="row">
+    @foreach ($data as $element)
+    <div class="search">
+      <div class="col">
         <a class="submit-card" href="{{ route('view-unit', $element->id) }}">
           <div class="library-card card-view">
             <h3 class="searchName">{{ $element->name }}</h3>
             <p>{{ $element->description }}</p>
-            <small>{{ $element->created_at }}</small> 
+            <small>{{ $element->created_at }}</small>
           </div>
         </a>
       </div>
     </div>
+    @endforeach
   </div>
 </div>
-@endforeach
